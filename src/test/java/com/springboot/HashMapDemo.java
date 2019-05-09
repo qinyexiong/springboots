ae85b6ff11564f581e8e806aa6d938e3c44cbaeb
qinyexiong <10939985092qq.com>
2019-05-09 19:51:41 +0800

    更新简历，上传个服务器工具

---
+++ src/test/java/com/springboot/HashMapDemo.java
@@ -0,0 +1,20 @@
+package com.springboot;
+
+import java.util.HashMap;
+
+/**
+ * @Auther: qinyx
+ * @Date: 2019/5/7/0007 09:52
+ * @Description:
+ */
+public class HashMapDemo {
+    
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("s","31");
+        hashMap.put("s","30");
+        System.out.println("控制台输出:"+hashMap.get("s"));
+
+        }
+    
+}
--- src/test/java/com/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/springboot/SpringbootApplicationTests.java
@@ -1,5 +1,7 @@
 package com.springboot;
 
+import java.util.Scanner;
+
 // @RunWith(SpringRunner.class)
 // @SpringBootTest
 public class SpringbootApplicationTests {
@@ -7,5 +9,20 @@ public class SpringbootApplicationTests {
     //	@Test
     //	public void contextLoads() {
     //	}
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        //定义一个标记变量
+        boolean flag=false;
+        for (int i = 2; i <num ; i++) {
+            if (num%i==0) {
+                flag=true;
+            }
+        }
+        String str=flag?"不是质数":"是质数";
+         System.out.println("输出控制台:"+str);
+
+    }
 
 }
